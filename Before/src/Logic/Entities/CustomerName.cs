efb5baa7f0855db7e2767e0c06d4b544cde3b11e
selvakumar <selvakumarappadurai93@gmail.com>
2020-01-15 23:10:44 +0530

    Added separate class for name and email of an customer

---
+++ Before/src/Logic/Entities/CustomerName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logic.Entities
+{
+    public class CustomerName : ValueObject<CustomerName>
+    {
+        public string Value { get; }
+        public CustomerName(string value)
+        {
+            Value = value;
+        }
+
+        protected override bool EqualsCore(CustomerName other)
+        {
+            return Value.Equals(other.Value, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
